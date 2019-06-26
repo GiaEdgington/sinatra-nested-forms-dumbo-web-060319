c3395dc7b86b7f94b145bd5ce0e0f6dce4606a1a
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-06-26 00:47:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :new
+    end 
+
+    #Create Action
+    post '/pirates' do
+      
+      @pirate = params[:pirate]
+      ##@ships = params[:ship]
+      erb :show 
+    end
+
+    #Show Action
   end
 end
